cc9030f009cdec9cbcc48da62a1214b8c3d03e9b
StrategFirst <mathieu.toulon@free.fr>
2021-04-21 09:33:09 +0200

    Improve : improve recap html css

--- resources/views/recap.blade.php
+++ resources/views/recap.blade.php
@@ -10,6 +10,7 @@
     </head>
 
     <body>
+        <h2> Récapitulatif </h2>
         <p>Vous avez choisi <strong>Licence 3 INFORMATIQUE</strong></p>
         <p>Avec les options suivantes : 
         <ul>
@@ -19,11 +20,31 @@
 
         <section id="info">
             Vous pouvez retrouver sur le campus : 
-            <a href="https://www.univ-angers.fr/fr/vie-des-campus/sport.html" target="_blank">SUAPS</a>
-            <a href="https://www.univ-angers.fr/fr/vie-des-campus/sante.html" target="_blank">SUMPS</a>
-            <a href="http://www.fneb.fr/corpo-sciences-angers/" target="_blank">Corpo</a>
-            <a href="https://projetcohesion.info/" target="_blank">PC[I]</a>
-            <a href="https://bu.univ-angers.fr/accueil" target="_blank">BU</a>
+            <ul>
+                <li>
+                    <a href="https://www.univ-angers.fr/fr/vie-des-campus/sport.html" target="_blank">
+                        <abbr title="Service Universitaire d'Activité Physique et Sportive">SUAPS</abbr>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.univ-angers.fr/fr/vie-des-campus/sante.html" target="_blank">
+                        <abbr title="Service Universitaire de Médecine Préventive et la Santé">SUMPS</abbr>
+                    </a>
+                </li>
+                <li><a href="http://www.fneb.fr/corpo-sciences-angers/" target="_blank">Corpo Science Angers</a></li>
+                <li>
+                    <a href="https://projetcohesion.info/" target="_blank">
+                        <abbr title="Projet et Cohésion en Informatique">PC[I]</abbr>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://bu.univ-angers.fr/accueil" target="_blank">
+                        <abbr title="Bibliotheques Universitaire">BU</abbr>
+                    </a>
+                </li>
+            </ul>
         </section>
+
+        <p> Votre groupe vous sera communiquer prochainement </p>
     </body>
 </html> 
