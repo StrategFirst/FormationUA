72715b6148daac061481e2731dd4ca6191245699
StrategFirst <mathieu.toulon@free.fr>
2021-04-25 15:17:00 +0200

    Style css connexion admin

--- resources/views/adminConnexion.blade.php
+++ resources/views/adminConnexion.blade.php
@@ -5,16 +5,14 @@
         <title>Laravel</title>
 
         <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
-       
+        <link rel="stylesheet" href="{{asset('css/adminConnexion.css')}}"/>
 
     </head>
 
     <body>
-
         <form action="{{ route('admin') }}" method="GET">
-            <input type="text" name="token" placeholder="token">
+            <input type="text" name="token" placeholder="Token">
             <input type="submit" name="" value="Envoyer">
         </form>
-        
     </body>
 </html>
