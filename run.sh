0bb620ad2870716c2a2aa289de4ded4869b8a2e9
StrategFirst <mathieu.toulon@free.fr>
2021-04-27 17:40:00 +0200

    Fix run

--- run.sh
+++ run.sh
@@ -1,11 +1,1 @@
-
-echo -e "\n\033[40;96m Arret des services en concurence avec Laravel \033[0m\n"
-sudo service mysql stop 2> /dev/null
-sudo service apache2 stop 2> /dev/null
-
-echo -e "\n\033[40;96m Lancement de Laravel \033[0m\n"
-./vendor/bin/sail up
-
-echo -e "\n\033[40;96m Relancement des services en concurence avec Laravel \033[0m\n"
-sudo service mysql start 2> /dev/null
-sudo service apache2 start 2> /dev/null
+php artisan serve --port 8000 --host 0.0.0.0
