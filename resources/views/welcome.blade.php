d300a02600b3618955537bbbacea2cd9acd610b0
StrategFirst <mathieu.toulon@free.fr>
2021-05-01 13:15:37 +0200

    Use .env variables ( blade syntax ) for page title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="fr-FR" dir="ltr">
     <head>
         <meta charset="utf-8"/>
-        <title> Formation UA </title>
+        <title> {{config('app.name')}} </title>
 
         <link rel="stylesheet" href="{{asset('css/app.css')}}"/>
         <link rel="stylesheet" href="{{asset('css/home.css')}}"/>
