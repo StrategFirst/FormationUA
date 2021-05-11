312e8a21fc2bb01869c4f8aef30fab35ea55a6d3
StrategFirst <mathieu.toulon@free.fr>
2021-05-11 22:58:30 +0200

    Factorisation via la vue créé précédement

--- app/Models/CONVERSION.php
+++ app/Models/CONVERSION.php
@@ -17,24 +17,7 @@ class CONVERSION extends Model
                 return [ $couple->id_etu , $couple->id_mat ];      
             }
             ,
-                array_merge(
-                    DB::select(
-                        "SELECT choix_etudiants.id_etu AS id_etu, appartenance_matiere.id_sous_matiere AS id_mat
-                        FROM choix_etudiants
-                            JOIN appartenance_matiere ON choix_etudiants.id_matiere = appartenance_matiere.id_matiere;"
-                        ,[]
-                    )
-                    ,
-                    DB::select(
-                        "SELECT inscrit_formation.id_etudiant AS id_etu, appartenance_matiere.id_sous_matiere AS id_mat
-                         FROM inscrit_formation
-                            JOIN ue ON inscrit_formation.id_formation = ue.id_formation
-                            JOIN matiere ON ue.id = matiere.id_ue
-                            JOIN appartenance_matiere on matiere.id = appartenance_matiere.id_matiere;"
-                        ,[]
-                    )
-                )
-            
+            DB::select("SELECT * FROM suivi_etudiant_sous_matiere")    
         );
         $suivi_fichier = fopen( '../repartition/data/suivi.csv' , 'w+' );
         foreach( $suivi as $ligne ) {
