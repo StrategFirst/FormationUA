dd5b8be5ec3df09539bbce3349beed1c801378af
tomy49123 <tomy.renaudin@laposte.net>
2021-05-17 18:09:01 +0200

    Refonte de l'algo de repartition (affichage seul)

--- repartition/repartition.cpp
+++ repartition/repartition.cpp
@@ -2,131 +2,145 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <array>
+#include <algorithm>
 
-const float SEUIL_NV_GROUPE = 0.0;
-
-using id_t = unsigned int;
-enum class cour_t {CM, TD, TP, CMTD};
-struct Suivi {
-	id_t id_etu;
-	id_t id_mat;
-};
-struct Contrainte {
-	id_t id_mat;
-	cour_t type;
-	size_t max_etu;
-};
-
-void affichageFichier(const std::vector<Suivi> & suivis,const std::vector<Contrainte> & contraintes) {
-	for(Suivi s:suivis) {
-		std::cout << "\033[92m" << s.id_etu << "\033[0m,\033[93m" << s.id_mat << "\033[0m" << std::endl;
+using LigneCSV = std::vector<std::string>;
+using FichierCSV = std::vector<LigneCSV>;
+LigneCSV parseCSV(std::string line) {
+	LigneCSV resultat;
+	size_t begin = 0;
+	while(begin < line.length()) {
+		size_t end = line.find(',',begin);
+		if(end == std::string::npos) {
+			resultat.push_back(line.substr(begin,line.length()));break;}
+		resultat.push_back(line.substr(begin,end-begin));
+		begin = end + 1;
 	}
-	for(Contrainte c:contraintes) {
-		std::cout << "\033[93m" << c.id_mat << "\033[0m,\033[96m" << c.max_etu << "\033[0m,\033[95m" << (int)c.type << "\033[0m" << std::endl;
-	}
+	return resultat;
 }
-std::vector<Suivi> lectureSuivi() {
-	std::vector<Suivi> liste;
-	std::ifstream fichier("data/suivi.csv");
-	if(fichier.is_open()) {
+FichierCSV getCSV(std::string fileName) {
+	std::ifstream fichier( fileName.c_str() );
+	if( fichier.is_open() ) {
+		FichierCSV result;
 		char* chars = new char[256];
-		while(!  fichier.eof() ) {
+		while( ! fichier.eof() ) {
 			fichier.getline(chars,256);
 			std::string line(chars);
-			if(line.length() > 0) {
-				int pos = line.find(',');
-				Suivi s;
-				s.id_etu = std::stoi(line.substr(0,pos));
-				s.id_mat = std::stoi(line.substr(pos+1,line.length()));
-				liste.push_back(s);
-			}
+			result.push_back(parseCSV(line));
 		}
 		delete[] chars;
-		return liste;
+		return result;
 	} else {
-		throw "An error occured while openning the file!";
+		throw std::invalid_argument("An error occured while openning the file!");
 	}
 }
-std::vector<Contrainte> lectureContrainte() {
-	std::vector<Contrainte> liste;
-	std::ifstream fichier("data/contrainte.csv");
-	if(fichier.is_open()) {
-		char* chars = new char[256];
-		while(! fichier.eof() ) {
-			fichier.getline(chars,256);
-			std::string line(chars);
-			if(line.length() > 0) {
-				Contrainte c;
-				int posa = line.find(',');
-				c.id_mat = std::stoi(line.substr(0,posa));
-				line = line.substr(posa+1,line.length());
-				int posb = line.find(',');
-				c.max_etu = std::stoi(line.substr(posb+1,line.length()));
-				std::string t = line.substr(0,posb);
-				if(t == "TP") {
-					c.type = cour_t::TP;
-				} else if(t == "TD") {
-					c.type = cour_t::TD;
-				} else if(t == "CM") {
-					c.type = cour_t::CM;
-				} else if(t == "CM-TD") {
-					c.type = cour_t::CMTD;
-				} else {
-					throw std::invalid_argument("Type de cours non reconnu : [" + t + "]");
-				}
-				liste.push_back(c);
-			}
+std::ostream& operator<<(std::ostream & os,const FichierCSV & file) {
+	for(auto const & line:file) {
+		bool k = true;
+		for(auto const & column:line) {
+			if(k)
+				os << " " << column << " | ";
+			else
+				os << " " << column << " | ";
+			k = !k;
 		}
-		delete[] chars;
-		return liste;
-	} else {
-		throw "An error occured while openning the file!";
+		os << std::endl;
 	}
+	return os;
 }
 
-void test(const std::vector<Suivi> & suivis,const std::vector<Contrainte> & contraintes) {
-	for(Contrainte c:contraintes) {
-		std::vector<id_t> suive;
-		for(Suivi s:suivis) {
-			if(s.id_mat == c.id_mat) {
-				suive.push_back(s.id_etu);
+struct Etudiant {
+	size_t id_etu;
+	std::vector<size_t> id_mat;
+};
+struct Parcour {
+	std::vector<size_t> id_etu;
+	std::vector<size_t> id_mat;
+};
+
+const int TAILLE_GRP_MAX = 5;
+
+struct Parcours {
+	std::vector<std::array<size_t,TAILLE_GRP_MAX>> etudiants;
+	std::vector<size_t> id_mat;
+};
+using Info = std::vector<Parcours>;
+
+void getEtudiant() {
+	FichierCSV csv = getCSV("data/suivi.csv");
+	std::vector<Etudiant> liste;
+	for(auto & ligne:csv) {
+		bool find = false;
+		for(auto & etudiant:liste) {
+			if(etudiant.id_etu == std::stoi(ligne.at(0))) {
+				etudiant.id_mat.push_back(std::stoi(ligne.at(1)));
+				find = true;
+				break;
 			}
 		}
-		float k = ( (float)suive.size() ) / ( (float)c.max_etu);
-		int grp_count = k;
-		k = k - grp_count;
-		if( k > SEUIL_NV_GROUPE) {
-			grp_count++;
+		if( !find ) {
+			Etudiant t;
+			t.id_etu = std::stoi(ligne.at(0));
+			t.id_mat.push_back( std::stoi(ligne.at(1)) );
+			liste.push_back( t );
 		}
-
-		std::vector<std::vector<id_t>> Liste;
-		Liste.reserve(grp_count);
-		for(int i=0 ; i<grp_count ; i++) {
-			Liste.push_back({});
+	}
+	std::vector<Parcour> result;
+	for(auto & etudiant:liste) {
+		bool find = false;
+		for(auto & parcour:result) {
+			if( std::is_permutation(etudiant.id_mat.begin(),etudiant.id_mat.end(),parcour.id_mat.begin())) {
+				parcour.id_etu.push_back( etudiant.id_etu );
+				find = true;
+				break;
+			}
 		}
-
-		int grp_i = 0;
-		for(auto etu:suive) {
-			Liste[grp_i].push_back(etu);
-			grp_i = (grp_i + 1)%grp_count;
+		if( !find ) {
+			Parcour p;
+			p.id_etu.push_back( etudiant.id_etu );
+			p.id_mat = etudiant.id_mat;
+			result.push_back(p);
 		}
+	}
+	std::cout << result.size() << std::endl;
 
-		// Affichage
-		std::cout << "	Pour la matière " << c.id_mat << " de type " << (int)c.type << " : " << std::endl;
-		for(int groupe=0 ; groupe<grp_count ; groupe++) {
-			std::cout << "\t" << "Pour le groupe " << groupe+1 << ":" << std::endl << "\t\t";
-			for(auto etu:Liste[groupe]) {
-				std::cout << etu << ";";
+	Info data;
+	for(auto & parcours:result) {
+		Parcours p;
+		p.id_mat = parcours.id_mat;
+		int pos = TAILLE_GRP_MAX;
+		for(auto & etudiants:parcours.id_etu) {
+			if(pos >= TAILLE_GRP_MAX) {
+				p.etudiants.push_back( std::array<size_t,TAILLE_GRP_MAX>() );
+				pos = 0;
+			}
+			p.etudiants.back()[pos] = etudiants;
+			pos++;
+		}
+		data.push_back(p);
+	}
+
+	/////// AFFFICHAGE
+	for(auto p:data) {
+		std::cout << "Pour les étudiants du parcours avec les matières : " <<std::endl<<"\t";
+		for(auto m:p.id_mat) {
+			std::cout << m << ";";
+		}
+		std::cout << std::endl  << std::endl;
+		for(auto g:p.etudiants) {
+			std::cout << "\t\t - ";
+			for(auto e:g) {
+				std::cout << "[" << e << "]";
 			}
 			std::cout << std::endl;
 		}
+		std::cout << std::endl << "________________________________________" << std::endl;
 	}
 }
 
 int main() {
-	auto suivi = lectureSuivi();
-	auto contrainte = lectureContrainte();
-	affichageFichier(suivi,contrainte);
-	test(suivi,contrainte);
+	FichierCSV temp = getCSV("data/suivi.csv");
+	getEtudiant();
 	return EXIT_SUCCESS;
 }
