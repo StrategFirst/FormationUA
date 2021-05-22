5f4a5a1f7bb286455a69ea9f5122c57a7ddfa75b
StrategFirst <mathieu.toulon@free.fr>
2021-05-22 06:58:32 +0200

    create file for custom type

---
+++ repartition/type.hh
@@ -0,0 +1,18 @@
+// Chaque cours a un ou plusieurs type :
+enum class TypeCours { CM , CMTD , TD , TP };
+
+// Une matière sera de manière général représenté par son identifiant et pour chacun d'entre eux
+//  on peut affecter une paire de clé(unique) valeurs correspondant a un type/format de cours ainsi que son volume horaire
+using Matiere = std::map<size_t,std::map<TypeCour,size_t>>;
+
+// Une classe est un ensemble de groupe (dont le cardinal est borné de 0 à X, X étant prédéfini) qui suit un ensemble de matière
+// Un groupe est un ensemble d'étudiant (dont le cardinal est borné de 0 à TAILLE_GRP_MAX, TAILLE_GRP_MAX étant une constante ajustable)
+// Un étudiant est ici représenté par un id unique qui lui a été attribué
+// Une matière est ici représenté par un id unique qui lui a été attribué et un type de cours (td,tp,cm,...)
+struct Classe {
+	std::vector<std::array<size_t,TAILLE_GRP_MAX>> etudiants;
+	std::vector<std::pair<size_t,TypeCours>> matiere_suivi;
+};
+
+// Une promotion est un ensemble de classe
+using Promotion = std::vector<Classe>;
