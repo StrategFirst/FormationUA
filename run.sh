3bbfffe614e28bfd1f4d198a54173329123dfbda
StrategFirst <mathieu.toulon@free.fr>
2021-05-23 03:24:53 +0200

    Add background tasks system and precompiling Change output to log directory Make it more beautiful

--- run.sh
+++ run.sh
@@ -1,1 +1,24 @@
-php artisan serve --port 8000 --host 0.0.0.0
+# Predefine console color
+colorUA="\033[1;38;2;10;187;239m"
+colorRESET="\033[0m"
+
+# Compile
+echo -e "$colorUA ► Compiling programs $colorRESET"
+cd repartition/
+./compile
+cd ../
+
+# Create log file if it doesn't exist
+mkdir logs -p
+
+prefixLogFile=$(date +%F)
+
+# Run
+echo -e "$colorUA ► Starting server $colorRESET"
+php artisan serve --port 8000 --host 0.0.0.0 > logs/$prefixLogFile-serv.log & \
+echo -e "$colorUA ► Starting background tasks $colorRESET" &&
+php artisan queue:work --timeout=600 --tries=1 > logs/$prefixLogFile-task.log && fg
+
+
+
+
