c07170c567f44b2717d651632c8f887bc22bdbe7
StrategFirst <mathieu.toulon@free.fr>
2021-05-24 07:16:45 +0200

    Utilisation du modèle de token

--- app/Http/Middleware/APIAccessRestrict.php
+++ app/Http/Middleware/APIAccessRestrict.php
@@ -2,31 +2,22 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
+use App\Models\TOKEN;
 
-use Illuminate\Support\Facades\DB;
+use Closure;
 
 class APIAccessRestrict {
 
    public function handle($request, Closure $next)
    {
-		$k = 0;
 		if( isset($_POST['token']) || isset($_GET['token']) ) {
-			$k = 1;
 			if( isset($_GET['token']) ) {
 				$token = $_GET['token'];
 			} else {
 				$token = $_POST['token'];
 			}
-			$query = DB::select('SELECT access_level FROM token WHERE token.token = :tk',['tk'=>$token]);
-			$k = 2;
-			if(sizeof($query) > 0) {
-				$k = 3;
-				$perm = $query[0]->access_level;
-				if($perm > 0) {
-					$k = 5;
-					return $next($request);
-				}
+			if(TOKEN::is_token_api($token)) {
+				return $next($request);
 			}
 		}
 
