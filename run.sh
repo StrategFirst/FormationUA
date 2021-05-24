06fdc7ff72cdc9c2baf476a1eb2fd2dbb03c42f2
StrategFirst <mathieu.toulon@free.fr>
2021-05-24 05:06:35 +0200

    Upgrade, le rendu, passage en français et  meilleurs gestion des process

--- run.sh
+++ run.sh
@@ -1,23 +1,56 @@
-# Predefine console color
+#!/bin/bash
+
+# Couleur console
 colorUA="\033[1;38;2;10;187;239m"
+colorOK="\033[1;38;2;69;255;69m"
+colorERR="\033[1;38;2;255;69;69m"
+colorLOG="\033[1;38;2;255;255;40m"
+colorGREY="\033[2;38;3;25;25;25m"
 colorRESET="\033[0m"
 
-# Compile
-echo -e "$colorUA ► Compiling programs $colorRESET"
+#Texte marco
+encour="\033[5m. . .\033[0m"
+valide="$colorOK✓$colorRESET"
+
+# Compilation
+echo -e -n "$colorUA ► Compilation de(s) programme(s) $encour $colorRESET \r"
 cd repartition/
 ./compile
 cd ../
+echo -e "$colorUA ► Compilation de(s) programme(s) terminé(s) ! $colorRESET $valide"
 
-# Create log file if it doesn't exist
-mkdir logs -p
+# Création repertoires log
+if [[ ! -d "./logs" ]]
+then
+	echo -e "$colorUA ► Création d'un répertoire de log $colorRESET"
+	mkdir logs -p
+fi
 
 prefixLogFile=$(date +%F)
 
-# Run
-echo -e "$colorUA ► Starting server $colorRESET"
-php artisan serve --port 8000 --host 0.0.0.0 >> logs/$prefixLogFile-serv.log & \
-echo -e "$colorUA ► Starting background tasks $colorRESET" &&
-php artisan queue:work --timeout=600 --tries=1 >> logs/$prefixLogFile-task.log && fg
+# Lancement
+echo -e -n "$colorUA ► Lancement du serveur web $encour $colorRESET \r"
+pidServerWeb=$(php artisan serve --port 8000 --host 0.0.0.0 >> logs/$prefixLogFile-serv.log & echo $!)
+echo -e "$colorUA ► Serveur web lancer $colorRESET$valide$colorGREY ( logs/$prefixLogFile-serv.log ) $colorRESET "
+echo -e -n "$colorUA ► Lancement du serveur des taches de fond $encour $colorRESET \r"
+pidServerTask=$(php artisan queue:work --timeout=600 --tries=1 >> logs/$prefixLogFile-task.log & echo $!)
+echo -e "$colorUA ► Serveur de taches de fond lancer $colorRESET$valide$colorGREY ( logs/$prefixLogFile-task.log ) $colorRESET "
+
+# Arrêt
+trap " \
+echo -e -n \"\n$colorUA ► Arrêt du serveur $encour\r\"; \
+kill $pidServerTask 2> /dev/null ;\
+kill $pidServerWeb 2> /dev/null ;\
+echo -e \"$colorUA ► Serveur correctement arrêter ! \$valide \"; \
+exit \
+" SIGINT
+
+echo -e "$colorUA ► Serveur opérationnel $valide"
+
+# Attente
+while true; do
+	: # Attente d'interruption
+done
 
 
 
