d422174d7fcdf8934243059e6c87171a0eb014d3
StrategFirst <mathieu.toulon@free.fr>
2021-05-26 16:44:01 +0200

    Frontend extract CSV

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,7 @@
 function updateInfo(aside) {
 
 	aside.innerHTML = '';
-	fetch( `${window.location.href}/api` )
+	fetch( `./api` )
 		.then( response => {
 			if(response.status === 200) {
 				return response.json();
@@ -27,3 +27,30 @@ function updateInfo(aside) {
 }
 
 updateInfo( document.querySelector('aside') );
+
+
+document.getElementById('recup-csv').onclick = (event) => {
+	let x = document.createElement('a');
+	let y = document.createElement('a');
+	let z = document.createElement('a');
+
+	x.href= './recup/etudiants.csv';
+	y.href= './recup/matieres.csv';
+	z.href= './recup/formation.csv';
+
+	x.style.display = 'none';
+	y.style.display = 'none';
+	z.style.display = 'none';
+
+	document.body.appendChild(x);
+	document.body.appendChild(y);
+	document.body.appendChild(z);
+
+	const delay = ( a ) => { a.click(); document.body.removeChild(a); };
+	setTimeout( delay , 000 , x);
+	setTimeout( delay , 400 , y);
+	setTimeout( delay , 800 , z);
+
+	event.preventDefault();
+	return false;
+}
