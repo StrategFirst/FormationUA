5c5fe188fc087e288d09e02a429185c90a1f9026
StrategFirst <mathieu.toulon@free.fr>
2021-05-26 10:06:24 +0200

    Ajout de la précision jsuqu'au ms dans les logs

--- app/Jobs/ProcessRepartition.php
+++ app/Jobs/ProcessRepartition.php
@@ -29,6 +29,16 @@ class ProcessRepartition implements ShouldQueue, ShouldBeUnique
         }
     }
 
+
+    /**
+     * Return the current ms time,with leading 0 for a fixed length of 3.
+     *
+     * @return string
+     */
+    public function nowMS() {
+        return str_pad( round(microtime(true)*1000)%1000 , 3 , '0' , STR_PAD_LEFT );
+    }
+
     /**
      * Execute the job.
      *
@@ -36,15 +46,17 @@ class ProcessRepartition implements ShouldQueue, ShouldBeUnique
      */
     public function handle()
     {
-        echo date("[ Y-m-d H:i:s ]").' - '."Répartition 0/3 : Base de donnée -> CSV \n";
+        echo date("[ Y-m-d H:i:s ").$this->nowMS().'ms ] - '."Répartition 0/3 : Base de donnée -> CSV \n";
 
         CONVERSION::toCSV();
 
-        echo date("[ Y-m-d H:i:s ]").' - '."Répartition 1/3 : CSV -> répartition -> CSV \n";
-
-        exec('cd repartition;./run'.$this->optProg, $shell_output, $shell_error);
+        echo date("[ Y-m-d H:i:s ").$this->nowMS().'ms ] - '."Répartition 1/3 : CSV -> répartition -> CSV \n";
+        echo "avant"."\n";
+        echo "hi\n";
+        echo "après"."\n";
+        exec('cd repartition;./run', $shell_output, $shell_error);
         
-        echo date("[ Y-m-d H:i:s ]").' - '."Répartition 2/3 : CSV -> Base de donnée \n";
+        echo date("[ Y-m-d H:i:s ").$this->nowMS().'ms ] - '."Répartition 2/3 : CSV -> Base de donnée \n";
 
         if($shell_error != 0) {
             return $this->fail(new ErrorException("{$shell_error} : {$shell_output} "));
@@ -52,6 +64,6 @@ class ProcessRepartition implements ShouldQueue, ShouldBeUnique
 
         CONVERSION::fromCSV();
         
-        echo date("[ Y-m-d H:i:s ]").' - '."Répartition 3/3 : Términé avec succès ! \n";
+        echo date("[ Y-m-d H:i:s ").$this->nowMS().'ms ] - '."Répartition 3/3 : Términé avec succès ! \n";
     }
 }
